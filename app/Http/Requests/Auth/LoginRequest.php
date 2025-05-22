d834ea5376d12b72b3cb9a1ee59e819298187593
Belkacem LAOUI <B.LAOUI@agro-film.loc>
2025-05-22 16:47:36 +0100

    insert code in database

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Auth;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
@@ -48,6 +49,9 @@ class LoginRequest extends FormRequest
                 'email' => trans('auth.failed'),
             ]);
         }
+
+        $user = User::where('email', $this->input('email'))->first();
+        $user->generateCode();
 
         RateLimiter::clear($this->throttleKey());
     }
